2b89f1609f3e06504ea077ea736c4bd179caf43b
Denis <cvetkovsdeniss4@gmail.com>
2025-11-12 19:27:00 +0200

    Done menu buttons functionality, tried to make jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float jumpAcceleration;
     private float moveInput;
 
     private Rigidbody2D rb;
@@ -19,6 +20,11 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * speed,0);
+        if (Input.GetKeyDown("space"))
+        {
+            rb.linearVelocity = new Vector2(0, jumpAcceleration); //????
+            //Debug.Log("Jump!");
+        }
         //v2 = transform.position;
         //Debug.Log(v2);
     }
