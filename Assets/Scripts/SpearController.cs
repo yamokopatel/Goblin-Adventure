d9afc7061f2c27210b2124c3bb3aead0c5b932d8
Denis <cvetkovsdeniss4@gmail.com>
2025-12-08 12:46:44 +0200

    Upgraded spear controller

--- Assets/Scripts/SpearController.cs
+++ Assets/Scripts/SpearController.cs
@@ -3,31 +3,33 @@ using UnityEngine;
 public class SpearController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private int existTime;
+    private float existTime;
     public float hSpeed;
     private float direction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        existTime = 3000;
+        existTime = 3000f;
         rb = GetComponent<Rigidbody2D>();
         direction = System.Convert.ToSingle(GameObject.Find("Player").GetComponent("direction"));
     }
 
     private void FixedUpdate()
     {
-        if(existTime >= 0)
+        if(existTime > 0)
         {
             rb.linearVelocity = Vector2.right * direction * hSpeed;
+            existTime--;
         }
         else
         {
             rb.linearVelocity = Vector2.up * existTime;
-            if(existTime < -8)
+            if(existTime < -5)
             {
                 Destroy(this.gameObject);
             }
+            existTime -= 0.2f;
         }
-        existTime--;
+        
     }
 }
